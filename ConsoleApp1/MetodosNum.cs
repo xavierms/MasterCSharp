a1c6f64a33fabc4328358caf5a4991feab064f2d
xavierms <x.mejia@sisalril.gov.do>
2021-07-22 10:47:35 -0400

    Init comm

---
+++ ConsoleApp1/MetodosNum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEtodosYProdiedadesDeString
+{
+    class MetodosNum
+    {
+        static void Main1 (string[] args)
+        {
+            //***TryParse() recibe la constante a convertir y un out y variable donde se pasa lo convertido
+            //string numero = "15";
+            //int resultado;
+            //bool exito = Int32.TryParse(numero, out resultado);
+            //int suma = resultado + 5;
+            //Console.WriteLine("La suma es: " + suma);
+            // --------------------------------------------------------------------------------------//
+            //Equals(int) 
+            //int a = 30;
+            //int b = 20;
+            //if (a.Equals(b))
+            //{
+            //    Console.WriteLine("son iguales");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("son difernetes");
+            //}
+            //// --------------------------------------------------------------------------------------//
+            //ToString() convierte a cadena
+
+            // --------------------------------------------------------------------------------------//
+            //Math.Floor(decimal) borra la parte deciaml de un numero
+            //Math.Ceiling(decimal) devuelve el decimal mas pequeño
+            //Math.Round(decimal) redondea un valor a entero
+            //Math.Pow(double, double)devuelve un numero elevado a la potencia
+            //Math.Sqrt(double)obtiene la raiz cuadrada
+            //Math.max(valor1, valor2)devuelve el mayor de dos numeros 
+            //Math.min(valor1, valor2)devuelve el menor de dos numeros
+            //Math.Abs(valor1)calcula el decimal de un numero
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //****ToCharArray() Agarra la cadena y obtiene cada uno de sus caracteres y lo almacena en un array
+            //string nombre = "pepe come rico";
+            //char[] array = nombre.ToCharArray();
+            //int c = 0;
+
+            //foreach (int item in array)
+            //{
+            //    if (item.Equals('e')) c++;                
+            //}
+
+            //Console.WriteLine("Se repite la letra e " + c + " veces");
+            // --------------------------------------------------------------------------------------//
+
+            //****Substring(indiceInicio, IndiceFin) Toma las letras que elija desde el inicio al final ejemplo: 
+
+            // string passport = "12345678";
+            // string nombre = "Pepe Lucho";
+
+            //string past= passport.Substring(0, 3);
+            // string nomb =nombre.Substring(0, 3);
+
+            // Console.WriteLine("COD Passport: " + past + nomb);
+            // --------------------------------------------------------------------------------------//
+
+            //****Remove(IndiceInicio, IndiceFin) Elimina una porcion de una cadena, recibe un indice y un contador ejemplo:
+
+            //string nuevo = "A0FWOHWR";
+
+            //Console.WriteLine("New string: " + nuevo.Remove(0, 2)) ;
+
+            // --------------------------------------------------------------------------------------//
+
+            //****StartsWith(string) me devuelve el valor string que le asigne
+
+
+            //string aver = "pollo";
+            //if (aver.StartsWith("o"))
+            //{
+            //    Console.WriteLine("Inicia con P ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No inicia con p");
+            //}
+            //Console.WriteLine("LEtra: " + aver.StartsWith("p"));
+            //string[] nombres= { "Pepe", "Pedro", "Damian" };
+            //int c = 0;
+            //foreach (string item in nombres)
+            //{
+            //    if (item.StartsWith("D")) c++;
+
+            //}
+            //Console.WriteLine("Numero de veces " + c);
+
+            // --------------------------------------------------------------------------------------//
+
+            //EndsWith() lo contrario a StarWith()
+
+            //string apellido = "Romam";
+
+            //if (apellido.EndsWith("a"))
+
+            //{
+            //    Console.WriteLine("Termina con a");
+            //}
+            //else
+            //{
+            //    string ultimaLetra = apellido.Substring(apellido.Length -2, 1);
+            //    Console.WriteLine("No termia con m , termina con " + ultimaLetra);
+            //}
+
+            // --------------------------------------------------------------------------------------//
+            //****Contains()
+            //****Split(devuelve un array stirng) sepra las palabras
+
+            //string word = "CartucheraDba";
+
+            //string[] array = word.Split('a');
+            //foreach (string item in array)
+            //{
+            //    Console.Write("  " +item);
+            //}
+
+           
+        }
+    }
+}
